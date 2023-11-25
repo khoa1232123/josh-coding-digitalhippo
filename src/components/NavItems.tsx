c7c066752f6fc986de299dc8505028e01a530f4c
khoa1232123 <khoa1232123@gmail.com>
2023-11-25 23:45:50 +0700

    create base navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/configs";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
 
 type Props = {};
 
@@ -12,8 +13,24 @@ const NavItems = (props: Props) => {
 
   const navRef = useRef<HTMLDivElement | null>(null);
 
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
